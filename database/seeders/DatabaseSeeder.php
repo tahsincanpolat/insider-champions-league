bfd3bc428e292c66210eaf5e38a0e18669c7c7ef
tahsincanpolat <tahsincanpolat@gmail.com>
2024-11-07 01:08:00 +0300

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Team;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Team::insert([
+            ['name' => 'Arsenal'],
+            ['name' => 'Liverpool'],
+            ['name' => 'Manchester City'],
+            ['name' => 'Chelsea'],
         ]);
     }
 }
