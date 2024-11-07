6213eef3052409ee6941b9ebe711737c429baf42
tahsincanpolat <tahsincanpolat@gmail.com>
2024-11-07 16:18:39 +0300

    helper change

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -2,7 +2,7 @@
 
 namespace App\Helpers;
 
-class FixtureHelper
+class Helper
 {
     public static function generateRoundRobinFixtures($teams)
     {
@@ -47,5 +47,38 @@ class FixtureHelper
         }
 
         return $fixtures;
+    }
+    public static function playMatch($match)
+    {
+        $homeScore = rand(0, 5);
+        $awayScore = rand(0, 5);
+
+        $match->update([
+            'home_team_goal' => $homeScore,
+            'away_team_goal' => $awayScore,
+        ]);
+
+        $homeTeam = $match->homeTeam;
+        $awayTeam = $match->awayTeam;
+
+        if ($homeScore > $awayScore) {
+            $homeTeam->points += 3;
+        } elseif ($homeScore < $awayScore) {
+            $awayTeam->points += 3;
+        } else {
+            $homeTeam->points += 1;
+            $awayTeam->points += 1;
+        }
+
+        $homeTeam->goals_scored += $homeScore;
+        $homeTeam->goals_conceded += $awayScore;
+        $homeTeam->goal_difference = $homeTeam->goals_scored - $homeTeam->goals_conceded;
+
+        $awayTeam->goals_scored += $awayScore;
+        $awayTeam->goals_conceded += $homeScore;
+        $awayTeam->goal_difference = $awayTeam->goals_scored - $awayTeam->goals_conceded;
+
+        $homeTeam->save();
+        $awayTeam->save();
     }
 }
